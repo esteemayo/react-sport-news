2bb5af4c522f7689a3ab2f37c5805228e3116a23
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-17 02:15:44 +0100

    added header to app

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,55 @@
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setLogout } from 'features/auth/authSlice';
+import './header.css';
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => ({ ...state.auth }));
+
+  const handleLogout = () => {
+    dispatch(setLogout());
+  };
+
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <Link to='/'>Sport News</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/news'>News</Link>
+          </li>
+          {user && (
+            <>
+              <li>
+                <Link to='/news/add'>Add News</Link>
+              </li>
+              <li>
+                <Link to='/auth/dashboard'>Dashboard</Link>
+              </li>
+            </>
+          )}
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          {user ? (
+            <button className='btn-secondary' onClick={handleLogout}>
+              Logout
+            </button>
+          ) : (
+            <li>
+              <Link to='/auth/login' className='btn-secondary'>
+                Login
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
