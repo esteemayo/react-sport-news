cc3aa1a0eee54c81d3e4c8d8e996eb3a08130e6c
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-18 01:07:58 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const newsInputs = [
+  {
+    id: 'name',
+    type: 'text',
+    name: 'name',
+    label: 'Name',
+  },
+  {
+    id: 'date',
+    type: 'date',
+    name: 'date',
+    label: 'Date',
+  },
+  {
+    id: 'time',
+    type: 'time',
+    name: 'time',
+    label: 'Time',
+  },
+  {
+    id: 'file',
+    type: 'file',
+    name: 'file',
+    label: 'Image',
+  },
+];
+
+export const registerInputs = [
+  {
+    id: 'name',
+    type: 'text',
+    name: 'name',
+    label: 'Name',
+    placeholder: 'Name',
+  },
+  {
+    id: 'username',
+    type: 'text',
+    name: 'username',
+    label: 'Username',
+    placeholder: 'Username',
+  },
+  {
+    id: 'email',
+    type: 'email',
+    name: 'email',
+    label: 'Email',
+    placeholder: 'user@example.com',
+  },
+  {
+    id: 'password',
+    type: 'password',
+    name: 'password',
+    label: 'Password',
+    placeholder: '********',
+  },
+  {
+    id: 'passwordConfirm',
+    type: 'password',
+    name: 'passwordConfirm',
+    label: 'Confirm Password',
+    placeholder: '********',
+  },
+];
+
+export const loginInputs = [
+  {
+    id: 'email',
+    type: 'email',
+    name: 'email',
+    label: 'Email',
+    placeholder: 'user@example.com',
+  },
+  {
+    id: 'password',
+    type: 'password',
+    name: 'password',
+    label: 'Password',
+    placeholder: '********',
+  },
+];
