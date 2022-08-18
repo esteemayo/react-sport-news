ed5fdd8fc45d735b9834670ebdfb7eb3b35e0f37
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-18 01:53:09 +0100

    first commit

---
+++ src/services/sportService.js
@@ -0,0 +1,26 @@
+import http from './httpService';
+
+const apiEndpoint = '/sports';
+
+const sportUrl = (sportId) => {
+  return `${apiEndpoint}/${sportId}`;
+};
+
+export const getSports = () => http.get(`${apiEndpoint}`);
+
+export const searchSport = (query) =>
+  http.get(`${apiEndpoint}/search?searchQuery=${query}`);
+
+export const getUserSports = () => http.get(`${apiEndpoint}/user-sports`);
+
+export const getSportById = (sportId) => http.get(sportUrl(sportId));
+
+export const getSportBySlug = (slug) =>
+  http.get(`${apiEndpoint}/details/${slug}`);
+
+export const createSport = (sport) => http.post(apiEndpoint, sport);
+
+export const updateSport = (sportId, sport) =>
+  http.patch(sportUrl(sportId), sport);
+
+export const deleteSport = (sportId) => http.delete(sportUrl(sportId));
