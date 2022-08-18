c044c472c08023637520cdc6cedd151335ea59e3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-18 01:14:13 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import About from './about/About';
+import AddNews from './add/AddNews';
+import Dashboard from './dashboard/Dashboard';
+import EditNews from './edit/EditNews';
+import Home from './home/Home';
+import Layout from './layout/Layout';
+import Login from './login/Login';
+import News from './news/News';
+import NotFound from './notFound/NotFound';
+import Register from './register/Register';
+import SharedLayout from './layout/SharedLayout';
+import SingleNews from './singleNews/SingleNews';
+
+export {
+  About,
+  AddNews,
+  Dashboard,
+  EditNews,
+  Home,
+  Layout,
+  Login,
+  News,
+  NotFound,
+  Register,
+  SharedLayout,
+  SingleNews,
+};
