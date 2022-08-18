579de6dff74bdc2b20f8a47fad0cb46450d8b041
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-18 01:12:20 +0100

    added a reusable component

---
+++ src/components/textarea/TextArea.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const TextArea = ({ name, label, ...rest }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <textarea {...rest} name={name} id={name}></textarea>
+    </div>
+  );
+};
+
+TextArea.propTypes = {
+  name: PropTypes.string,
+  label: PropTypes.string,
+};
+
+export default TextArea;
