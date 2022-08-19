49a128330bef0a787bdda1a357610f37993f289e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-19 15:14:48 +0100

    applied lazy loading

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,16 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+import { lazy, Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Spinner from 'components/spinner/Spinner';
-import NewsItem from 'components/newsItem/NewsItem';
 import { getNews, reset } from 'features/news/newsSlice';
+
+const NewsItem = lazy(() => import('components/newsItem/NewsItem'));
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { news, isError, isLoading, message } = useSelector((state) => ({
+  const { news, isError, message } = useSelector((state) => ({
     ...state.news,
   }));
 
@@ -20,10 +21,6 @@ const Home = () => {
     return () => dispatch(reset());
   }, [isError, message, dispatch]);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
-
   if (news.length < 1) {
     return <h3>No News</h3>;
   }
@@ -31,9 +28,11 @@ const Home = () => {
   return (
     <div>
       <h1>Latest News</h1>
-      {news.map((item) => {
-        return <NewsItem key={item._id} {...item} />;
-      })}
+      <Suspense fallback={<Spinner />}>
+        {news.map((item) => {
+          return <NewsItem key={item._id} {...item} />;
+        })}
+      </Suspense>
       {news.length > 0 && (
         <Link to={'/news'} className='btn-secondary'>
           View All News
